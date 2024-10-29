01d9f81fb8a679252f53e159e6c7b146f9a3ffaf
idyll <liushun968858@gmail.com>
2024-10-29 14:26:10 +0800

    Add files via upload

--- M.sh
+++ M.sh
@@ -3,7 +3,9 @@
 MODPATH="/data/adb/modules/Screen"
 [[ ! -e ${MODDIR}/ll/log ]] && mkdir -p ${MODDIR}/ll/log
 source "${MODPATH}/scripts/X.sh"
+su -c am broadcast -a android.intent.action.ACTION_OPTIMIZE_DEVICE
 su -c am broadcast -a android.intent.action.SET_SCREEN_REFRESH_RATE -e refreshRate 120 -e fpsMode fixed
+
 
 
 
